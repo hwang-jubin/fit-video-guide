79954727dd7e3f34b16c189edeb306d6addcaa55
황주빈 <hjb06288@gmail.com>
2024-11-19 14:46:06 +0900

    feat: 운동 목록 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,55 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import Header from "./components/header";
+import Category from "./components/category";
+import Excercise from "./components/exercise";
+
+export type dataProps = {
+  id: string; // 'e780be0c-5d2e-4727-9d37-f0425f4979e7'와 같은 UUID 형식의 문자열
+  video_title: string; // '옆으로 누워 안쪽다리 올리기'
+  video_url: string; // 'http://openapi.kspo.or.kr/web/video/0AUDLJ08S_00135.mp4'
+  length_seconds: number; // 67
+  description: string; // '무릎관련 질환자를 위한 단계별 표준운동 1단계 - 옆으로 누워 안쪽다리 올리기'
+  production_year: string; // '2021'
+  thumbnail_urls: string[]; // ['http://openapi.kspo.or.kr/web/image/0AUDLJ08S_00135/0AUDLJ08S_00135_SC_00001.jpeg', ...]
+  thumbnail_url: string; // 'http://openapi.kspo.or.kr/web/image/0AUDLJ08S_00135/0AUDLJ08S_00135_SC_00001.jpeg'
+  age_group: string; // '공통'
+  created_at: string; // '2022-10-26T00:00:00+00:00'
+  training_type: string | null; // null or string (예: '유산소', '근력')
+  training_purpose: string | null; // null or string
+  support_tool: string; // '매트'
+  fitness_factor: string | null; // null or string
+  training_part: string; // '무릎'
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [data, setData] = useState<dataProps[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/exercise"); // 유효한 API URL 확인 필요
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const jsonData = await res.json();
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className=" w-[1440px]">
+      <Header />
+      <div className="pt-20 px-5">
+        <Category />
+        <div className="mx-6 grid grid-cols-3 gap-x-16 gap-y-11 mt-6">
+          {data.map((data) => (
+            <Excercise key={data.id} {...data} />
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
