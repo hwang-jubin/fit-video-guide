d8f0e17fa49e1a8c0773510376cccb7776da2cae
황주빈 <hjb06288@gmail.com>
2024-11-26 17:28:38 +0900

    feat: cookie 함수, token 검증로직 분리

---
+++ lib/cookie.ts
@@ -0,0 +1,6 @@
+import { cookies } from "next/headers";
+
+export default function getCookie() {
+  const cookieStore = cookies();
+  return cookieStore;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import getCookie from "@/lib/cookie";
 import db from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import tokenAuthentication from "./app/components/tokenAuthentication";
 
 interface Routes {
   [key: string]: boolean;
@@ -21,12 +22,13 @@ export async function middleware(request: NextRequest) {
   console.log(exists);
 
   // 토큰을 사용하여 사용자 정보 조회
-  const { data, error } = await db.auth.getUser(token);
-  console.log(data.user?.email);
-  // 사용자 정보가 없거나 오류가 있으면 로그인 페이지로 리디렉션
-
-  if (data.user) {
-    if (exists) return NextResponse.redirect(new URL("/", request.url));
+  if (token) {
+    const data = await tokenAuthentication(token);
+    if (data.user) {
+      if (exists) return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    //user의 profile page에 못들어가게 하기
   }
 
   // 기본적으로 요청을 허용
