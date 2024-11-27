e79863e9d70d56d91aa683e8448f0609345f2d67
황주빈 <hjb06288@gmail.com>
2024-11-27 17:41:46 +0900

    feat: access token 이 만료되면 access token, refresh token을 재발급

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import getCookie from "@/lib/cookie";
 import db from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import tokenAuthentication from "./app/components/tokenAuthentication";
+
+import tokenGenerate from "./app/components/auth/token-generate";
+import tokenAuthentication, {
+  generateNewAccessToken,
+} from "./app/components/auth/tokenAuthentication";
 
 interface Routes {
   [key: string]: boolean;
@@ -15,20 +19,25 @@ const publicOnlyUrls: Routes = {
 export async function middleware(request: NextRequest) {
   // 쿠키에서 토큰을 가져옵니다.
   const cookie = await getCookie();
-  const token = (await cookie).get("access_token")?.value;
+  const access_token = (await cookie).get("access_token")?.value;
+  const refresh_token = (await cookie).get("refresh_token")?.value;
 
   // 요청된 URL이 publicOnlyUrls에 존재하는지 확인
   const exists = publicOnlyUrls[request.nextUrl.pathname];
-  console.log(exists);
 
   // 토큰을 사용하여 사용자 정보 조회
-  if (token) {
-    const data = await tokenAuthentication(token);
-    if (data.user) {
+  if (access_token) {
+    const data = await tokenAuthentication(access_token);
+    if (data?.user) {
       if (exists) return NextResponse.redirect(new URL("/", request.url));
     }
-  } else {
-    //user의 profile page에 못들어가게 하기
+  } else if (access_token === undefined && refresh_token) {
+    const newAccess_token = await generateNewAccessToken(refresh_token);
+
+    tokenGenerate(
+      newAccess_token.session?.access_token!,
+      newAccess_token.session?.refresh_token!
+    );
   }
 
   // 기본적으로 요청을 허용
