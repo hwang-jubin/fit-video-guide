e11c81e6e0864c514b961012cd26f01403dbf723
황주빈 <hjb06288@gmail.com>
2024-12-03 15:24:22 +0900

    feat: 맞춤 동영상

--- app/api/exercise/route.ts
+++ app/api/exercise/route.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { getAuthSupabase } from "@/lib/auth";
 import { db } from "@/lib/db";
 // import { getCreateClient } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,11 +8,11 @@ import { NextRequest, NextResponse } from "next/server";
 const mapCategoryToTrainingPart = (category: string) => {
   const categoryMap: Record<string, string> = {
     "허리 운동": "허리",
-    "가슴 운동": "가슴",
     "어깨 운동": "어깨",
     "무릎 운동": "무릎",
     "짐볼 활용 운동": "짐볼",
     "밴드 활용 운동": "밴드",
+    "맞춤 동영상": "맞춤 동영상",
   };
 
   return categoryMap[category] || "default";
@@ -20,7 +21,6 @@ const mapCategoryToTrainingPart = (category: string) => {
 export async function GET(req: NextRequest) {
   const queryString = await req.nextUrl.searchParams.get("category");
   const category = mapCategoryToTrainingPart(queryString as string);
-  // const db = await getCreateClient();
 
   if (category === "default") {
     const { data: videos, error } = await db.from("videos").select("*");
@@ -37,12 +37,51 @@ export async function GET(req: NextRequest) {
       .eq("training_part", category);
 
     return NextResponse.json(videos);
-  } else {
+  } else if (category === "밴드" || category === "짐볼") {
     const { data: videos, error } = await db
       .from("videos")
       .select("*")
       .eq("support_tool", category);
 
     return NextResponse.json(videos);
+  } else if (category === "맞춤 동영상") {
+    const supabase = await getAuthSupabase();
+    const { data, error } = await supabase.auth.getUser();
+
+    if (data.user?.email) {
+      const { data: training_purpose, error: resultError } = await supabase
+        .from("user_info")
+        .select("training_purpose")
+        .eq("email", data.user.email)
+        .single();
+
+      if (training_purpose?.training_purpose === "체중 감량") {
+        const { data: videos, error } = await supabase
+          .from("videos")
+          .select("*")
+          .eq("training_purpose", "체력증진");
+
+        return NextResponse.json(videos);
+      } else if (training_purpose?.training_purpose === "근력 증진") {
+        const { data: videos, error } = await db
+          .from("videos")
+          .select("*")
+          .eq("training_purpose", "근력강화");
+
+        return NextResponse.json(videos);
+      } else if (training_purpose?.training_purpose === "유연성 증진") {
+        const { data: videos, error } = await supabase
+          .from("videos")
+          .select()
+          .eq("training_purpose", "유연성");
+
+        return NextResponse.json(videos);
+      }
+    } else {
+      return NextResponse.json(
+        { redirect: "/login" },
+        { status: 307 } // 리디렉션 상태 코드 추가
+      );
+    }
   }
 }
--- app/components/category.tsx
+++ app/components/category.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import ExcerciseList from "./exerciseList";
 import { dataProps } from "../page";
+import { useRouter } from "next/navigation";
 
 const categories = [
   "전체",
@@ -9,20 +10,23 @@ const categories = [
   "어깨 운동",
   "짐볼 활용 운동",
   "밴드 활용 운동",
+  "맞춤 동영상",
 ];
 
 export default function Category() {
   const [selectedCategory, setCategory] = useState<string>("전체");
   const [data, setData] = useState<dataProps[]>();
+  const route = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(`/api/exercise/?category=${selectedCategory}`); // 유효한 API URL 확인 필요
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
+      if (res.status === 307) {
+        route.push("/login");
+      } else {
+        const jsonData = await res.json();
+        setData(jsonData);
       }
-      const jsonData = await res.json();
-      setData(jsonData);
     };
 
     fetchData();
@@ -35,8 +39,12 @@ export default function Category() {
           <button
             onClick={() => setCategory(category)}
             className={`p-2 rounded-md w-auto ${
-              selectedCategory === category
-                ? "bg-white shadow-lg" // 선택된 카테고리 스타일
+              category === "맞춤 동영상"
+                ? selectedCategory === "맞춤 동영상"
+                  ? "bg-white shadow-lg" // 연파랑 (선택된 맞춤동영상 스타일)
+                  : "bg-blue-400 text-white" // 기본 파랑 (맞춤동영상 스타일)
+                : selectedCategory === category
+                ? "bg-white shadow-lg" // 선택된 다른 카테고리 스타일
                 : "bg-black text-white" // 기본 스타일
             }`}
             key={index}
