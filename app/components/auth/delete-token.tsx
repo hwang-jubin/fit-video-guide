478f0a6a6e74ae668460e6efa0774a942197fe8c
황주빈 <hjb06288@gmail.com>
2024-12-05 17:19:15 +0900

    feat: test1

--- app/components/auth/delete-token.tsx
+++ app/components/auth/delete-token.tsx
@@ -4,11 +4,6 @@ import { getAuthSupabase } from "@/lib/auth";
 import { cookies } from "next/headers";
 
 export default async function deleteToken() {
-  const cookieStore = cookies();
-  (await cookieStore).delete("refresh_token");
-  (await cookieStore).delete("access_token");
-
   const supabase = await getAuthSupabase();
-  const sign = supabase.auth.signOut();
-  console.log(sign);
+  const sign = await supabase.auth.signOut();
 }
