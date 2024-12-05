aa0ba780b1e89d89c9ffa1842208823d89c23d9b
황주빈 <hjb06288@gmail.com>
2024-12-05 22:59:28 +0900

    feat: dark mode 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen bg-white text-black `}
       >
         <Header />
         {children}
